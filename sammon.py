ec1c251c94bf5459eee24a162b529d677eccf62c
DidierGarciaToquiantzi <124199143+DidierGarciaToquiantzi@users.noreply.github.com>
2023-11-05 23:36:33 -0600

    Update sammon.py

--- sammon.py
+++ sammon.py
@@ -49,3 +49,37 @@ cbar.set_label('Clases')
 
 # Mostrar el gráfico 3D
 plt.show()
+
+# Calcular la matriz de similitud inicial en el espacio de alta dimensión
+def calculate_initial_similarity_matrix(X):
+    n = X.shape[0]
+    similarity_matrix = np.zeros((n, n))
+    for i in range(n):
+        for j in range(i + 1, n):
+            distance = np.linalg.norm(X[i] - X[j])
+            similarity_matrix[i, j] = 1 / (1 + distance)
+            similarity_matrix[j, i] = similarity_matrix[i, j]
+    return similarity_matrix
+
+# Función para calcular el estrés de Sammon
+def sammon_stress(Y, S, n):
+    stress = 0
+    for i in range(n):
+        for j in range(i + 1, n):
+            if S[i, j] > 0:
+                stress += ((np.linalg.norm(Y[i] - Y[j]) - S[i, j]) ** 2) / S[i, j]
+    return stress
+
+np.random.seed(0)
+n, m = X.shape
+Y = np.random.rand(n, 2)
+
+# Calcular la matriz de similitud inicial en el espacio de alta dimensión
+S = calculate_initial_similarity_matrix(X)
+
+# Hiperparámetros del algoritmo
+learning_rate = 0.1
+max_iterations = 100
+tolerance = 1e-5
+
+prev_stress = sammon_stress(Y, S, n)
